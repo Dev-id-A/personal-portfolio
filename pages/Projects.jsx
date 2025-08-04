bfa775c1e0f13a480536f88ffd03eaa12f1de1a9
Dev-id-A <davidaranzana618@gmail.com>
2025-08-04 11:47:33 +0200

    ProjectData has been filled in

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -25,7 +25,7 @@ function Projects({fadeLan, changeLan }) {
 
 
       <section id="projects-section" className="grid grid-cols-6 place-items-center mt-20 gap-6">
-        <Project divClass={"col-span-2"}></Project>
+        <Project changeLan={changeLan} divClass={"col-span-2"}></Project>
         <Project divClass={"col-span-2"}></Project>
         <Project divClass={"col-span-2"}></Project>
         <Project divClass={"col-span-3"}></Project>
--- src/assets/Project/Project.jsx
+++ src/assets/Project/Project.jsx
@@ -1,9 +1,14 @@
-function Project({projectName, description, divClass}) {
+function Project({projectName, description, divClass, changeLan}) {
   return (
     <div id="project" className={`border h-130 w-full ${divClass}`}>
         <img className="flex border h-70 w-full self-center"/>
         <h1 className="text-center text-3xl mt-5">Project name</h1>
         <p className="p-8 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi assumenda reiciendis dolorum iusto totam sit delectus magni qui sed et autem velit, consectetur officia culpa eius saepe ratione temporibus voluptas?</p>
+
+        <div className="flex flex-cols justify-center gap-60"> 
+          <a href="https://github.com" target="_blank" rel="noopener noreferrer"><p>{changeLan == "es" ? "Enlace al código":"Code Link"}</p></a>
+          <a href="https://github.com" target="_blank" rel="noopener noreferrer"><p>{changeLan == "es" ? "Enlace al proyecto":"Proyect Link"}</p></a>
+        </div>
     </div>
   )
 }
