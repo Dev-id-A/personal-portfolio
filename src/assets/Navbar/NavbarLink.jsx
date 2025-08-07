659c02d65954f4ec77034efd2fabbcd69a377440
Dev-id-A <davidaranzana618@gmail.com>
2025-08-07 19:07:03 +0200

    Styled Navbar

--- src/assets/Navbar/NavbarLink.jsx
+++ src/assets/Navbar/NavbarLink.jsx
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom"
 
 function NavbarLink({to, fadeLan, children}) {
   return (
-    <Link to={to} className={`w-40 hover:scale-110 transition-transform duration-300 ease-in-out
+    <Link to={to} className={`relative w-50 text-center 
+      hover:scale-110 transition-transform duration-300 ease-in-out hover:text-white 
+      after:content-[""] after:origin-center after:absolute after:block after:scale-x-0 after:bg-white
+      after:h-[2px] after:w-full after:transition-transform after:duration-300 hover:after:scale-x-50
       transition-opacity duration-300" ${fadeLan ? "opacity-0":"opacity-100"}`}>{children}</Link>
   )
 }
