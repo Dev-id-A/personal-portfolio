fc51ad593317abc1c2d4fadc44146c85c151e459
Dev-id-A <davidaranzana618@gmail.com>
2025-08-12 12:14:01 +0200

    hero section fully responsive

--- src/assets/Hero/Hero.jsx
+++ src/assets/Hero/Hero.jsx
@@ -2,14 +2,19 @@ import HeroBtn from "./HeroBtn";
 
 function Hero({changeLan, fadeLan}) {
   return (
-    <main id="hero-main" className={`grid grid-cols-1 place-items-center mt-20 transition-opacity duration-300" ${fadeLan ? "opacity-0":"opacity-100"}`}>
-        <img className="size-100 border" alt="David Aranzana" id="presentation-img"></img>
-        <h1 id="presentation" className="only-md:bg-black mt-10 text-5xl">
+    <main id="hero-main" className={`grid grid-cols-1 place-items-center mt-10 transition-opacity duration-300
+      sm:mt-15" 
+    ${fadeLan ? "opacity-0":"opacity-100"}`}>
+        <img className="md:size-100 size-50 border" alt="David Aranzana" id="presentation-img"></img>
+        <h1 id="presentation" className="mt-10 md:text-5xl text-xl text-center px-10">
             {changeLan == "es" ? "¡Hola! Soy David Aranzana, desarrollador Front-end":
             "Hi! I'm David Aranzana, Front-end developer"}
         </h1>
 
-        <h2  id="description" className="mt-18 text-2xl text-center px-100">
+        <h2  id="description" className="text-sm mt-8 text-center px-5
+        xl:px-100
+        lg-px-50
+        md:mt-18 md:text-2xl md:px-20">
           {changeLan == "es" ? `Soy un desarrollador web autodidacta con muchas ganas de aprender y seguir creciendo para dar el salto al mundo profesional. 
           Siempre me implico al máximo en cada uno de mis proyectos, cuidando que todo funcione bien, sea accesible y se vea bien en cualquier dispositivo.`:
 
@@ -17,7 +22,9 @@ function Hero({changeLan, fadeLan}) {
           give my best in every project, ensuring everything runs smoothly, is accessible, and responsive.`}
           </h2>
 
-          <section id="btn-section" className="grid grid-cols-3 gap-20 mt-18 pb-10">
+          <section id="btn-section" className="grid grid-cols-3 gap-5 mt-18 pb-10
+          sm:px-20 sm:gap-20
+          ">
 
             <HeroBtn  to="aboutMe">{changeLan == "es" ? "Sobre mí":"About me"}</HeroBtn>
             <HeroBtn  to="projects">{changeLan == "es" ? "Proyectos":"Projects"}</HeroBtn>
--- src/assets/Hero/HeroBtn.jsx
+++ src/assets/Hero/HeroBtn.jsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom"
 
 function HeroBtn({children, to}) {
   return (
-    <Link to={to}><button id="hero-btn" className="bg-[#483D8F] hover:shadow-[0_0_15px_white] border h-25 w-50 rounded-3xl text-2xl text-center 
+    <Link to={to}><button id="hero-btn" className="bg-[#483D8F] hover:shadow-[0_0_15px_white] border rounded-3xl text-center h-15 w-22 text-md 
+        lg:h-25 lg:w-50
+        md:h-20 md:w-40 md:text-2xl
         hover:cursor-pointer hover:scale-110 transition-all duration-300 ease-in-out"
         >{children}</button></Link>
   )
